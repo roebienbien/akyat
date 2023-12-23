006ec5ca1d3a365f6f3014bf55747580225d1a73
roebienbien <roebienbien@gmail.com>
2023-12-24 05:52:44 +0800

    feat: add change navbar on scroll

--- src/componnets/navbar/index.tsx
+++ src/componnets/navbar/index.tsx
@@ -1,26 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import HamburgerButton from './HamburgerButton';
 import { Navlinks } from './navlinks';
-import { GiHamburgerMenu } from 'react-icons/gi';
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = () => {
     setIsOpen((prevState) => !prevState);
   };
+
+  const [isScrolled, setIsScrolled] = useState(false);
+  const changeNavBackground = (e: any) => {
+    window.scrollY >= 1 ? setIsScrolled(true) : setIsScrolled(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeNavBackground);
+    return () => {
+      window.removeEventListener('scroll', changeNavBackground);
+    };
+  }, []);
+
   return (
     // <div className='fixed top-0 z-50  flex w-full items-center justify-between px-40 py-6  '>
-    <div className='fixed top-0 z-50 grid h-20 w-full bg-[#446E7A]   py-4 text-white'>
+    // <div className='fixed top-0 z-50 grid h-20 w-full bg-[#446E7A]   py-4 text-white'>
+    <div
+      className={`${
+        isScrolled ? 'bg-[#446e7a] text-white' : 'bg-transparent text-white'
+      } duration-7000 fixed top-0 z-50 grid h-16 w-full bg-opacity-100 py-2 transition-all  `}>
       <div className='flex items-center justify-between  px-6 xs:px-10 lg:px-20 '>
-        <span className='cursor-pointer  text-lg font-semibold lg:text-2xl'>Akyat</span>
+        <span className='cursor-pointer text-lg  font-semibold lg:text-2xl'>Akyat</span>
         <ul className='hidden space-x-10  md:flex'>
           {Navlinks.map((item, key) => (
-            <li key={key} className='cursor-pointer capitalize  lg:text-xl'>
+            <li key={key} className='cursor-pointer capitalize hover:text-gray-400  lg:text-xl'>
               {item.title}
             </li>
           ))}
         </ul>
-        <button className='hidden border  px-8 py-2  md:block lg:text-lg'>Login</button>
+        <button className='hidden border   px-8 py-2  md:block lg:text-lg'>Login</button>
         <HamburgerButton isOpen={isOpen} toggleMenu={toggleMenu} />
       </div>
     </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,4 +1,8 @@
 export default function Hero() {
+  // const title = 'Your start, Your pace, Your, Summit'; //slogan
+  // const title = 'The Mountains are waiting to be conquered';
+  const title = 'The Mountains are waiting';
+  const subtitle = '';
   return (
     // <div className='  h-screen'>
     <div
@@ -7,17 +11,32 @@ export default function Hero() {
       }}
       className=' grid h-screen w-full place-items-center justify-items-center bg-cover bg-no-repeat text-white opacity-100'>
       <div className=''>
-        <h1 className='px-10 text-center font-poppins text-3xl font-semibold sm:px-20 sm:text-5xl md:text-6xl lg:text-8xl'>
-          Lorem, ipsum dolor <br className='hidden md:block' /> Lorem, ipsum
+        <h1 className='px-10 text-center font-poppins text-4xl font-semibold sm:px-20 sm:text-5xl md:text-6xl lg:text-7xl'>
+          {title}
         </h1>
         <div className='mx-auto mt-14 grid justify-items-center px-6 sm:px-32 '>
-          <p className='text-center text-xs sm:text-base lg:text-2xl'>
+          {/* <p className='text-center text-xs sm:text-base lg:text-2xl'>
             Linear is the better way Lorem, ipsum dolor. Lorem, ipsum. <br className='hidden md:block' /> to build products
             Lorem ipsum dolor sit amet.
-          </p>
-          <button className='mt-8  border px-6 py-2 lg:px-12 lg:py-4'>Explore</button>
+          </p> */}
+          <p className='text-center text-xl font-bold sm:text-2xl lg:text-2xl'>{subtitle}</p>
+          <button className='mt-8  border-2 px-6 py-2 text-base lg:px-12 lg:py-4'>Explore</button>
         </div>
       </div>
     </div>
   );
 }
+
+// <div className=''>
+//   <h1 className='px-10 text-center font-poppins text-3xl font-semibold sm:px-20 sm:text-5xl md:text-6xl lg:text-8xl'>
+//     Lorem, ipsum dolor <br className='hidden md:block' /> Lorem, ipsum
+//   </h1>
+//   <div className='mx-auto mt-14 grid justify-items-center px-6 sm:px-32 '>
+//     {/* <p className='text-center text-xs sm:text-base lg:text-2xl'>
+//       Linear is the better way Lorem, ipsum dolor. Lorem, ipsum. <br className='hidden md:block' /> to build products
+//       Lorem ipsum dolor sit amet.
+//     </p> */}
+//     <p className='text-center text-xl font-bold sm:text-2xl lg:text-2xl'>{slogan}</p>
+//     <button className='mt-8  border px-6 py-2 lg:px-12 lg:py-4'>Explore</button>
+//   </div>
+// </div>
