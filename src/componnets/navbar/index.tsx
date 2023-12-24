418061752cf851ca8b5a64d5a4ea7976f4f3b0df
roebienbien <roebienbien@gmail.com>
2023-12-25 04:58:34 +0800

    feat: add border on-hover on nav items

--- src/componnets/navbar/index.tsx
+++ src/componnets/navbar/index.tsx
@@ -29,9 +29,11 @@ function Navbar() {
       } duration-7000 fixed top-0 z-50 grid h-16 w-full bg-opacity-100 py-2 transition-all  `}>
       <div className='flex items-center justify-between  px-6 xs:px-10 lg:px-20 '>
         <span className='cursor-pointer text-lg  font-semibold lg:text-2xl'>Akyat</span>
-        <ul className='hidden space-x-10  md:flex'>
+        <ul className='hidden space-x-10 md:flex'>
           {Navlinks.map((item, key) => (
-            <li key={key} className='cursor-pointer capitalize hover:text-gray-400  lg:text-xl'>
+            <li
+              key={key}
+              className='cursor-pointer border-b-2 border-transparent capitalize text-gray-300 hover:border-white hover:text-white      lg:text-xl'>
               {item.title}
             </li>
           ))}
